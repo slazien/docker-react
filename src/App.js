b42bbea532641c917221e147a910ee834cf1eb9f
vagrant <vagrant@vagrant.vm>
2020-10-25 18:58:38 +0000

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed
+          Hi from Misiu <3
         </p>
         <a
           className="App-link"
